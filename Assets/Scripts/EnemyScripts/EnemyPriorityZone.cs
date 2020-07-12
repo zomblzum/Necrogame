8ca63b8b77f94315244591cf01726fb80be81457
zomblzum <zomblzum@yandex.ru>
2020-07-12 17:34:08 +0500

    меню правки компиляция

---
+++ Assets/Scripts/EnemyScripts/EnemyPriorityZone.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPriorityZone : MonoBehaviour
+{
+    [Header("Дистанция включения преследования игрока")]
+    public float aggroDistance = 50f;
+    [Header("Триггер зона для приоритетных миньнонов")]
+    public CapsuleCollider aggroAreaCollider;
+    [Header("Враг с приоритетной зоной")]
+    public Enemy enemy;
+
+    private List<GameObject> priorityTargets;
+
+    void Awake()
+    {
+        priorityTargets = new List<GameObject>();
+        aggroAreaCollider.radius = aggroDistance;
+    }
+
+    // Срабатывает при входе игрока и миньонов в область видимости врага
+    protected virtual void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<Minion>() != null || other.gameObject.GetComponent<Player>() != null)
+        {
+            priorityTargets.Add(other.gameObject);
+
+            if(!priorityTargets.Contains(enemy.GetCurrentTarget()))
+            {
+                enemy.SetPriorityTarget(other.gameObject);
+            }
+        }
+    }
+
+    // Срабатывает, когда игрок или его прислужник умирают/уходят из зоны видимости
+    protected virtual void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.GetComponent<Minion>() != null || other.gameObject.GetComponent<Player>() != null)
+        {
+            priorityTargets.Remove(other.gameObject);
+            priorityTargets.RemoveAll(item => item == null);
+
+            if (enemy.GetCurrentTarget() == other.gameObject)
+            {
+                enemy.GetClosestTarget();
+            }
+        }
+    }
+}
