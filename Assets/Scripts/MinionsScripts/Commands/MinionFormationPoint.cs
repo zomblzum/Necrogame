f6c346a4fc744a76c1b905d19516e20210892740
zomblzum <zomblzum@yandex.ru>
2020-09-25 21:23:09 +0500

    123

--- Assets/Scripts/MinionsScripts/Commands/MinionFormationPoint.cs
+++ Assets/Scripts/MinionsScripts/Commands/MinionFormationPoint.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 
 public class MinionFormationPoint : MonoBehaviour
 {
@@ -11,24 +14,26 @@ public class MinionFormationPoint : MonoBehaviour
 
     static public void drawString(string text, Vector3 worldPos, Color? colour = null)
     {
-        UnityEditor.Handles.BeginGUI();
+#if UNITY_EDITOR
+        Handles.BeginGUI();
 
         var restoreColor = GUI.color;
 
         if (colour.HasValue) GUI.color = colour.Value;
-        var view = UnityEditor.SceneView.currentDrawingSceneView;
+        SceneView view = SceneView.currentDrawingSceneView;
         Vector3 screenPos = view.camera.WorldToScreenPoint(worldPos);
 
         if (screenPos.y < 0 || screenPos.y > Screen.height || screenPos.x < 0 || screenPos.x > Screen.width || screenPos.z < 0)
         {
             GUI.color = restoreColor;
-            UnityEditor.Handles.EndGUI();
+            Handles.EndGUI();
             return;
         }
 
         Vector2 size = GUI.skin.label.CalcSize(new GUIContent(text));
         GUI.Label(new Rect(screenPos.x - (size.x / 2), -screenPos.y + view.position.height + 4, size.x, size.y), text);
         GUI.color = restoreColor;
-        UnityEditor.Handles.EndGUI();
+        Handles.EndGUI();
+#endif
     }
 }
